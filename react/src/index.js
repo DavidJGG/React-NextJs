0e00ddee4b1713a3d5d0071e2ac61c449ee5b07f
DavidJGGm <jonathangmz79@hotmail.com>
2024-05-25 18:30:18 -0600

    testing props and custom components

--- react/src/index.js
+++ react/src/index.js
@@ -2,11 +2,16 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import App from './App';
+import CategoryComponent from './categoryfield/CategoryComponent'
 
 const root = createRoot(document.getElementById("root"));
+
 root.render(
-  <StrictMode>
+  <StrictMode> 
+    <CategoryComponent message={"Hola a todos"} cls={"category"} />
+    <CategoryComponent message={"Hola a todos 2"} cls={"category2"} />
+    <CategoryComponent />
     <App />
   </StrictMode>
 );
